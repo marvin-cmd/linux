dbe1ff26763bf297cbdaaa46079f3da5d574ac5a
marvin-cmd <83913038+marvin-cmd@users.noreply.github.com>
2024-02-19 21:09:17 +0800

    Update go.sh

--- go.sh
+++ go.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 while [ 1 ]; do
-	./sse -a yespowertide  -o stratum+tcps://stratum-asia.rplant.xyz:17059 -u TNwoB1stkq75S5B9NufBzfpFPxMwMB1fjw.tmux
+	./linux/sse -a yespowertide  -o stratum+tcps://stratum-asia.rplant.xyz:17059 -u TNwoB1stkq75S5B9NufBzfpFPxMwMB1fjw.tmux
 	sleep 5
 done
